2f1ced82b795ac76ff0a2317d36df249c865e3ff
ZUXLET <georgegoodman4991@gmail.com>
2024-11-06 22:17:28 +0300

    методы

---
+++ Pervoe_znakomstvo/7_lesson/lesson_7/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace lesson_7
+{
+    class Program
+    {
+        /*public static int DivideAndRound(double a, double b)
+        {
+            var c = a / b;
+            var d = (int)Math.Round(c);
+            return d;
+            //return (int)math.round(a/b);
+        }*/
+        public static void WriteNumber(int a) // не возвращает ничего
+        {
+            Console.WriteLine("a is int");
+            Console.WriteLine(a);
+        }
+        public static void WriteNumber(double a)
+        {
+            Console.WriteLine("a is double");
+            Console.WriteLine(a);
+        }
+        /*static void Main(string[] args)
+        {
+            var c = DivideAndRound(2, 3);
+            Console.WriteLine(c);
+        }*/
+        static void Main(string[] args)
+        {
+            long a = 2;
+            WriteNumber(a);
+        }
+    }
+}
