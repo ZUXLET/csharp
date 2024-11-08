adb32fe2bdf3b44aaaf3a74f8e719637f575782e
ZUXLET <georgegoodman4991@gmail.com>
2024-11-08 16:11:07 +0300

    задачка на работу методов

---
+++ Pervoe_znakomstvo/8_lesson/lesson_8/Program.cs
@@ -0,0 +1,33 @@
+﻿class Lesson_8
+{
+    static void Main()
+    {
+	    Print(GetSquare(42));
+    }
+    static int GetSquare(int a)
+    {
+        return (int)Math.Pow(a, 2);
+    }	
+    static void Print(int a)
+    {
+	    Console.WriteLine(a);
+    }
+
+}
+
+
+
+
+
+/*Многие знают, что ответ на главный вопрос жизни, вселенной и всего такого — 42. Но Вася хочет большего! Он желает знать квадрат этого числа!
+
+Вы разделили с Васей работу — он написал главный метод Main, а вам осталось лишь дописать методы Print и GetSquare.
+
+Создайте два метода Print и GetSquare, так, чтобы код снизу заработал. Если забыли синтаксис определения методов — подсмотрите в видеолекции или в предыдущие задачи.
+
+public static void Main()
+{
+	Print(GetSquare(42));
+}
+1
+public static void Main()
