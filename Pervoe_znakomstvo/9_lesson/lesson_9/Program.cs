56be2e908b16843b271c90d3877c70251fce90d6
ZUXLET <georgegoodman4991@gmail.com>
2024-11-12 17:29:17 +0300

    задача на деление строки и удаление пробелов

---
+++ Pervoe_znakomstvo/9_lesson/lesson_9/Program.cs
@@ -0,0 +1,34 @@
+﻿class lesson_9
+{
+    static void Main()
+    {
+	    Console.WriteLine(GetLastHalf("I love CSharp!"));
+	    Console.WriteLine(GetLastHalf("1234567890"));
+	    Console.WriteLine(GetLastHalf("до ре ми фа соль ля си"));
+    }
+    static string GetLastHalf(string text)
+{
+    return text.Substring(text.Length / 2).Replace(" ", "");
+}
+    /*static string GetLastHalf(string text)
+    {
+        
+        return text.Replace( " ", "") text.Remove(0, text.Length/2);
+
+    }*/
+    public static string GetLastHalf(string text)
+    {
+        // Удаляем пробелы из строки
+        string noSpaces = text.Replace(" ", "");
+
+        // Находим индекс начала второй половины
+        int startIndex = noSpaces.Length / 2;
+
+        // Возвращаем вторую половину строки
+        return noSpaces.Substring(startIndex);
+    }
+}
+
+/*var c = "blya";
+var a = c.Length;
+Console.WriteLine(a);*/
