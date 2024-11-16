379a34d8a0ee04899ed03e7e6584a83701a80629
ZUXLET <georgegoodman4991@gmail.com>
2024-11-16 23:28:20 +0300

    гладиаторские бои

---
+++ csharp_baseLvl/2_uslovnie_operatori_i_cicly/dz_0.1/dz_01/Program.cs
@@ -0,0 +1,34 @@
+﻿//Гладиаторские бои
+Random rand = new Random();
+float health1 = rand.Next(90, 100);
+var damage1 = rand.Next(5, 20);
+var armor1 = rand.Next(25, 65);
+float health2 = rand.Next(70, 100);
+var armor2 = rand.Next(65, 100);
+var damage2 = rand.Next(20, 40);
+Console.WriteLine($"Гладиатор 1: {health1} здоровья, {damage1} урона, {armor1} брони.");
+Console.WriteLine($"Гладиатор 2: {health2} здоровья, {damage2} урона, {armor2} брони.");
+while(health1 > 0 && health2 > 0)
+{
+    health1 -= Convert.ToSingle(rand.Next(0, damage2)) / 100 * armor1;
+    health2 -= Convert.ToSingle(rand.Next(0, damage1)) / 100 * armor2;
+    Console.WriteLine("Здоровье первого гладиатора: "+ health1);
+    Console.WriteLine("Здоровье второго гладиатора: "+ health2);
+
+    if (health1 <= 0 && health2 <=0)
+    {
+        Console.WriteLine("Ничья, обам погибли.");
+        break;
+    }
+    else if (health1 <=0)
+    {
+        Console.WriteLine("Гладиатор под номером 1 пал смертью храбрых!");
+        break;
+    }
+    else if (health2 <= 0)
+    {
+        Console.WriteLine("Гладиатор под номером 2 пал смертью храбрых!");
+        break;
+    }
+    
+}
--- csharp_baseLvl/2_uslovnie_operatori_i_cicly/dz_0.1/dz_01/obj/Debug/net8.0/dz_01.AssemblyInfo.cs
+++ csharp_baseLvl/2_uslovnie_operatori_i_cicly/dz_0.1/dz_01/obj/Debug/net8.0/dz_01.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("dz_0")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("dz_01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44b477f0f8b84116c983488a1b8b85751faa6598")]
-[assembly: System.Reflection.AssemblyProductAttribute("dz_0")]
-[assembly: System.Reflection.AssemblyTitleAttribute("dz_0")]
+[assembly: System.Reflection.AssemblyProductAttribute("dz_01")]
+[assembly: System.Reflection.AssemblyTitleAttribute("dz_01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
