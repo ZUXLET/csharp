37ceaa14b92507d2a7fcc4574e7ac14bd1c59a5b
ZUXLET <georgegoodman4991@gmail.com>
2024-11-17 14:23:37 +0300

    освоение циклов

---
+++ csharp_baseLvl/2_uslovnie_operatori_i_cicly/dz_1/dz_1/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Задача:
+При помощи циклов вы можете повторять один и тот же код множество раз.
+Напишите простейшую программу которая выводит заданное пользователем сообщение заданное пользователем количество раз.*/
+var messege = "";
+var count = 0;
+Console.Write("Введите сообщение:");
+messege = Console.ReadLine();
+Console.Write("Сколько раз его вывести:");
+count = int.Parse(Console.ReadLine());
+while (count-- > 0)
+{
+    Console.WriteLine(messege);       
+}
